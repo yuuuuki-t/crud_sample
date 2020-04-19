4468ebb963b928f6f06f5cc5f59fd1cda8e8bb7f
yuki.t <cxntp@MacBook-Pro.local>
2020-04-19 14:04:25 -0700

    used resources

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,11 @@ class UsersController < ApplicationController
     User.create(user_params)
   end
 
+  # 詳細情報を表示するためのアクション
+  def show
+    @user = User.find(params[:id])
+  end
+
   # 編集画面に対応するアクション
   def edit
     @user = User.find(params[:id])
